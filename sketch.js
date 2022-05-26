436bc720b04dcae2b52bfbdc1485da98042c2437
Shengyang <wu1632@purdue.edu>
2022-05-26 00:51:46 -0400

    Added More Features

--- sketch.js
+++ sketch.js
@@ -19,22 +19,36 @@ class Cell {
 
 /* Define Maze Properties */
 
+let states;
 let maze;
 let height;
 let width;
 let cell_size;
 let path_found;
+let stack;
+let path;
+let animating;
 
-/* Variables Storing Inputs From the User */
+let MAX_FRAME;
+let MIN_FRAME;
+
+/* UI Variables */
 
 let input_height;
 let input_width;
 let text_height;
 let text_width;
+let text_maxframe;
+let text_minframe;
+let text_fps;
+
 let button_generate;
+let button_animate;
 
 let pathfind_menu;
 let button_pathfind;
+
+let slider_frame;
 
 /* Starting Point and Ending Point in the Maze */
 
@@ -42,6 +56,7 @@ let start_x;
 let start_y;
 let end_x;
 let end_y;
+let set_start = true;
 
 /* Colors */
 
@@ -52,173 +67,74 @@ let pink;
 function setup() {
   height = 20;
   width = 20;
-  start_x = 0;
-  start_y = height - 1;
-  end_x = 10;
-  end_y = 10;
+  start_x = -1;
+  start_y = -1;
+  end_x = -1;
+  end_y = -1;
+  
+  stack = [];
+  path = [];
   
   cell_size = 20;
   
   path_found = false;
+  animating = false;
+
+  MIN_FRAME = 1;
+  MAX_FRAME = 60;
   
   blue = color('blue');
   red = color('rgb(255,0,0)');
   pink = color('rgb(255,192,203)');
     
   input_text();
-  create_maze();
+  frameRate(MAX_FRAME / 2);
   
-  createCanvas(width * cell_size, height * cell_size);
+  var canvas = createCanvas(width * cell_size, height * cell_size);
+  create_maze();
+  print(maze);
+  generate_maze_whole();
 }
 
 function draw() {
   background(255);
+  frameRate(slider_frame.value());
+  if (animating) {
+    generate_maze_next();
+  }
+
   draw_maze();
-  
+
+  if (start_x != -1 && end_x != -1) {
+    find_path_dfs();
+    path_found = true;
+  }
+
   if (path_found) {
     draw_path();
   }
 }
 
-/*
-  This function draw text boxes, buttons, etc. that accept user inputs.
-*/
-
-function input_text() {
-  input_width = createInput();
-  input_width.position(width * cell_size + cell_size, 65);
-  
-  text_width = createElement('h3', 'Enter Width');
-  text_width.position(width * cell_size + cell_size, 28);
-  
-  text_height = createElement('h3', 'Enter Height');
-  text_height.position(width * cell_size + cell_size, 80);
-  
-  input_height = createInput();
-  input_height.position(width * cell_size + cell_size, 117);
-  
-  button_generate = createButton('Generate Maze');
-  button_generate.position(width * cell_size + cell_size, 145);
-  button_generate.mousePressed(resize_maze);
-  
-  pathfind_menu = createSelect();
-  pathfind_menu.position(width * cell_size + cell_size, 190);
-  pathfind_menu.option('Breadth-First Search');
-  pathfind_menu.option('Depth-First Search');
-  
-  button_pathfind = createButton('Find Path!');
-  button_pathfind.position(width * cell_size + cell_size, 215);
-  button_pathfind.mousePressed(find_path);
+function draw_rect(color, x, y) {
+  fill(color);
+  rect(x * cell_size, y * cell_size, cell_size, cell_size);
 }
 
-function find_path() {
-  clear_search_history();
-  if (pathfind_menu.value() == 'Breadth-First Search') {
-    find_path_bfs();
-  } else if (pathfind_menu.value() == 'Depth-First Search') {
-    find_path_dfs();
-  }
-  path_found = true;
-}
-
-/* 
-  Resize the Maze
-*/
-
-function resize_maze() {
-  height = input_height.value();
-  width = input_width.value();
-  print(height + " : " + width);
-  
-  if (height == "" || width == "") {
-    height = 15;
-    width = 15;
-  }
-  
-  text_width.position(width * cell_size + cell_size, 28);
-  text_height.position(width * cell_size + cell_size, 80);
-  input_width.position(width * cell_size + cell_size, 65);
-  input_height.position(width * cell_size + cell_size, 117);
-  button_generate.position(width * cell_size + cell_size, 145);
-  start_x = 0;
-  start_y = 0;
-  end_x = width - 1;
-  end_y = height - 1;
-  
-  create_maze();
-  
-  resizeCanvas(width * cell_size, height * cell_size);
-}
-
-
-/*
-  Draw the Maze based on the 2D array representation.
-*/
-
-function draw_maze() {
-  line(0, 0, width * cell_size, 0);
-  line(0, 0, 0, height * cell_size);
-  
-  noStroke();
-  fill(0, 0, 255, 100);
-  rect(start_x * cell_size, start_y * cell_size, cell_size, cell_size);
-  
-  fill(255, 0, 0, 100);
-  rect(end_x * cell_size, end_y * cell_size, cell_size, cell_size);
-  stroke(0, 0, 0);
-  
-  for (var i = 0; i < width; i++) {
-    for (var j = 0; j < height; j++) {
-      if (maze[i][j].searched) {
-        if (!(i == start_x && j == start_y) && !(i == end_x && j == end_y)) {
-          noStroke();
-          fill(255, 192, 103, 100);
-          rect(i * cell_size, j * cell_size, cell_size, cell_size)
-          stroke(0, 0, 0);
-        }
-        
-      }
-      
-      if (maze[i][j].right == false) {
-        line((i + 1) * cell_size, j * cell_size, (i + 1) * cell_size, j * cell_size + cell_size)
-      }
-
-      if (maze[i][j].down == false) {
-        line(i * cell_size, (j + 1) * cell_size, i * cell_size + cell_size, (j + 1) * cell_size);
-      }
-    }
-  }
-}
-
-/*
-  This function creates a random maze using randomized DFS algorithm.
-*/
-
-function create_maze() {
-  maze = new Array(width);
-  for (var i = 0; i < width; i++) {
-    maze[i] = new Array(height);
-  }
-
-  for (i = 0; i < width; i++) {
-    for (var j = 0; j < height; j++) {
-      maze[i][j] = new Cell(i, j);
-    }
-  }
-  
-  stack = [];
-  start = maze[0][0];
-  stack.push(start);
-  
-  while (stack.length > 0) {
-    
+function generate_maze_next() {
+  if (stack.length > 0) {
     current = stack.pop();
     current.visited = true;
-    //print("Current: " + current.x + " " + current.y);
     
-    next = find_next(current);
+    draw_rect(blue, current.x, current.y);
     
-    if (next != null) {
+    nexts = find_next(current);
+    
+    if (nexts != null) {
+      index = Math.floor(Math.random() * options.length)
+      next = options[index];
+      for (var i = 0; i < options.length; i++) {
+        draw_rect(red, options[i].x, options[i].y);
+      }
       stack.push(current);
       //print("Next: " + next.x + " " + next.y + "\n");
     
@@ -240,8 +156,206 @@ function create_maze() {
         }
       }
       stack.push(next); 
-    }   
+    }  
   }
+}
+
+function mouseClicked() {
+  if (mouseX < width * cell_size && mouseY < height * cell_size) {
+    if (set_start) {
+      start_x = parseInt(mouseX / cell_size);
+      start_y = parseInt(mouseY / cell_size);
+      set_start = false;
+    } else {
+      end_x = parseInt(mouseX / cell_size);
+      end_y = parseInt(mouseY / cell_size);
+      
+      if (end_x == start_x && end_y == start_y) {
+        end_x = -1;
+        end_y = -1;
+      }
+      set_start = true;
+    }
+  }
+}
+
+/*
+  This function draw text boxes, buttons, etc. that accept user inputs.
+*/
+
+function input_text() {
+  input_width = createInput();
+  
+  text_width = createElement('h3', 'Enter Width');
+  
+  text_height = createElement('h3', 'Enter Height');
+  
+  input_height = createInput();
+  
+  button_generate = createButton('Generate Maze');
+  button_generate.mousePressed(resize_maze);
+
+  button_animate = createButton('Animate Maze Generation');
+  button_animate.mousePressed(animate_maze);
+  
+  text_fps = createElement('h3', 'Animation Speed (fps)');
+  
+  slider_frame = createSlider(1, 60, 30, 1);
+  
+  text_maxframe = createElement('h5', MAX_FRAME);
+
+  text_minframe = createElement('h5', MIN_FRAME);
+  
+  position_elements();
+}
+
+function animate_maze() {
+  resize_maze();
+  create_maze();
+  animating = true;
+}
+
+/* 
+  Resize the Maze
+*/
+
+function resize_maze() {
+  start_x = -1;
+  start_y = -1;
+  end_x = -1;
+  end_y = -1;
+  path_found = false;
+
+  height = input_height.value();
+  width = input_width.value();
+  
+  if (height == "" || width == "") {
+    height = 20;
+    width = 20;
+  }
+  
+  position_elements();
+
+  create_maze();
+  generate_maze_whole();
+
+  resizeCanvas(width * cell_size, height * cell_size);
+}
+
+function position_elements() {
+  text_width.position(width * cell_size + cell_size, 0);
+  text_height.position(width * cell_size + cell_size, 52);
+  input_width.position(width * cell_size + cell_size, 42);
+  input_height.position(width * cell_size + cell_size, 94);
+  button_generate.position(width * cell_size + cell_size, 127);
+  
+  button_animate.position(width * cell_size + cell_size + 120, 127);
+
+  text_fps.position(width * cell_size + cell_size, 152);
+
+  slider_frame.position(width * cell_size + cell_size, 198);
+
+  text_maxframe.position(width * cell_size + cell_size + 140, 196);
+
+  text_minframe.position(width * cell_size + cell_size, 196);
+}
+
+/*
+  Draw the Maze based on the 2D array representation.
+*/
+
+function draw_maze() {
+  line(0, 0, width * cell_size, 0);
+  line(0, 0, 0, height * cell_size);
+  
+  if (start_x != -1) {
+    noStroke();
+    fill(0, 0, 255, 100);
+    rect(start_x * cell_size, start_y * cell_size, cell_size, cell_size);
+    stroke(0, 0, 0);
+  }
+
+  if (end_x != -1) {
+    noStroke();
+    fill(255, 0, 0, 100);
+    rect(end_x * cell_size, end_y * cell_size, cell_size, cell_size);
+    stroke(0, 0, 0);
+  }
+  
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
+      
+      if (!maze[i][j].visited) {
+        noStroke();
+        fill(255, 192, 103, 100);
+        rect(i * cell_size, j * cell_size, cell_size, cell_size)
+        stroke(0, 0, 0);
+      }
+      
+      if (maze[i][j].right == false) {
+        line((i + 1) * cell_size, j * cell_size, (i + 1) * cell_size, j * cell_size + cell_size)
+      }
+
+      if (maze[i][j].down == false) {
+        line(i * cell_size, (j + 1) * cell_size, i * cell_size + cell_size, (j + 1) * cell_size);
+      }
+    }
+  }
+}
+
+function generate_maze_whole() {
+  while (stack.length > 0) {
+    
+    current = stack.pop();
+    current.visited = true;
+    
+    nexts = find_next(current);
+    
+    if (nexts != null) {
+      index = Math.floor(Math.random() * options.length)
+      next = options[index];
+      stack.push(current);
+    
+      if (current.x == next.x) {
+        if (next.y < current.y) {
+          next.down = true;
+          current.up = true;
+        } else {
+          next.up = true;
+          current.down = true;
+        }
+      } else {
+        if (next.x < current.x) {
+          current.left = true;
+          next.right = true;
+        } else {
+          current.right = true;
+          next.left = true;
+        }
+      }
+      stack.push(next); 
+    }  
+  }
+}
+
+/*
+  This function creates a random maze using randomized DFS algorithm.
+*/
+
+function create_maze() {
+  maze = new Array(width);
+  for (var i = 0; i < width; i++) {
+    maze[i] = new Array(height);
+  }
+
+  for (i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
+      maze[i][j] = new Cell(i, j);
+    }
+  }
+  
+  start = maze[parseInt(width / 2)][parseInt(height / 2)];
+  stack.push(start);
 }
 
 /*
@@ -262,7 +376,6 @@ function find_next(cell) {
     if (x < 0 || x >= width || y < 0 || y >= height) {
       continue;
     }
-    
     if (!maze[x][y].visited) {
       options.push(maze[x][y]);
       //print("Unvisited: " + x + " " + y);
@@ -274,9 +387,7 @@ function find_next(cell) {
   }
 
   
-  index = Math.floor(Math.random() * options.length)
-  //print("Choose: " + options[index].x + " " + options[index].y);
-  return options[index];
+  return options;
 }
 
 /*
@@ -284,12 +395,12 @@ function find_next(cell) {
 */
 
 function find_path_dfs() {
-  stack = [];
+  clear_search_history();
   start = maze[start_x][start_y];
-  stack.push(start);
+  path.push(start);
   
-  while (stack.length > 0) {
-    current = stack.pop();
+  while (path.length > 0) {
+    current = path.pop();
     current.searched = true;
     
     //print(current.x + " " + current.y);
@@ -333,7 +444,7 @@ function find_path_dfs() {
       if (!maze[x][y].searched) {
         maze[x][y].searched = true;
         maze[x][y].previous = current;
-        stack.push(maze[x][y]);
+        path.push(maze[x][y]);
       }
     } 
   } 
@@ -400,7 +511,7 @@ function draw_path() {
   strokeWeight(4);
   while (true) {
     prev = c.previous;
-    line(c.x * cell_size + cell_size / 2, c.y * cell_size + cell_size / 2, prev.x * cell_size + cell_size / 2, prev.y * cell_size + cell_size / 2);
+    line(c.x * cell_size + cell_size/ 2, c.y * cell_size + cell_size / 2, prev.x * cell_size + cell_size / 2, prev.y * cell_size + cell_size / 2);
     if (prev.x == start_x && prev.y == start_y) {
       break;
     }
